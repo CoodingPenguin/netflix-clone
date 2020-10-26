4201092cd74149292e60f73d53b28541c3b96bcb
CoodingPenguin <unodostre@naver.com>
2020-10-26 17:18:58 +0900

    style: 💄 add styling at Header component

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -12,8 +12,10 @@ const globalStyles = createGlobalStyle`
     }
     body{
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-        font-size: 14px;
+        font-size: 12px;
         background-color: rgba(20, 20, 20, 1);
+        color: white;
+        padding-top: 50px;
     }
 `;
 
