defe2ed796d9e1c4118f68520a5d5ededa113836
CoodingPenguin <unodostre@naver.com>
2020-10-31 17:12:48 +0900

    Converting to Presenter-Container pattern done

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,4 +1,15 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
-export default class extends React.Component {}
+export default class extends React.Component {
+  state = {
+    result: null,
+    error: null,
+    loading: true,
+  };
+
+  render() {
+    const { result, error, loading } = this.state;
+    return <DetailPresenter result={result} error={error} loading={loading} />;
+  }
+}
