c933151fcd9952e3fd5d865bb41fca0be2f0fb56
CoodingPenguin <unodostre@naver.com>
2020-10-31 17:50:36 +0900

    Add detail route of movie and tv show

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -19,6 +19,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" exact component={TV} />
         <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
