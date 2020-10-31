3c5796b31057ee85d539c8e86d66e947673f7a16
CoodingPenguin <unodostre@naver.com>
2020-10-31 20:54:16 +0900

    Add styling to Poster component

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -13,7 +13,7 @@ const globalStyles = createGlobalStyle`
     body{
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
         font-size: 12px;
-        background-color: rgba(20, 20, 20, 1);
+        background-color: rgba(30, 30, 30, 1);
         color: white;
         padding-top: 80px;
     }
