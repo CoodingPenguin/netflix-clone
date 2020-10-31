47f72d4f5d32be53b6c2db56d4eb97c946cf12bc
CoodingPenguin <unodostre@naver.com>
2020-10-31 17:48:12 +0900

    Add api call at SearchContainer

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { movieApi, tvApi } from "../../api";
 
 export default class extends React.Component {
   state = {
@@ -7,7 +8,37 @@ export default class extends React.Component {
     tvResults: null,
     searchTerm: "",
     error: null,
-    loading: true,
+    loading: false,
+  };
+
+  // 1. searchTerm이 있는지 체크하고 있다면 데이터 로드
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  // 2. searchTerm을 갖고 있는 영화, TV쇼 데이터 로드
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      // 2-1. 데이터 로드
+      const {
+        data: { results: movieResults },
+      } = await movieApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+
+      // 2-2. state에 데이터 저장
+      this.setState({ movieResults, tvResults });
+    } catch {
+      this.setState({ error: "Can't find the results." });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
@@ -19,6 +50,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         error={error}
         loading={loading}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
